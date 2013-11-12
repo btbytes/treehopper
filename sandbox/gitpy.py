93dd06028d9705d10e661ea31e4f47de2c73d710
Pradeep Kishore Gowda <pradeep@btbytes.com>
2013-11-11 21:25:01 -0500

    work in progress

--- sandbox/gitpy.py
+++ sandbox/gitpy.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
 gitpy.py -- program to test out some gitpython api.
 '''
+import sys
 
-from git import *
+from git import Repo
+from string import strip
+from py2neo import neo4j, node, rel
+
+#import codecs
+# UTF8Writer = codecs.getwriter('utf8')
+# sys.stdout = UTF8Writer(sys.stdout)
+
 repo = Repo("/Users/pradeep/src/requests")
-heads = repo.heads
+graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
+heads = repo.heads
 commits = repo.iter_commits('master')
+count = 0
+
+
 for commit in commits:
-    print commit.author, commit.message
+    nc = {'hexsha': commit.hexsha,
+          'committer':unicode(commit.committer),
+          'message': unicode(strip(commit.message)),
+          'author': unicode(commit.author),
+          'committer': unicode(commit.committer),
+          'committed_date': commit.committed_date,
+          'message': unicode(commit.message)
+      }
+    graph_db.create(nc)
+
+    #for parent in commit.parents:
+    #    print '\t%s' % (parent, )
+    #print 'Tree: ', commit.tree
+    #print 'Authored Tree: ', commit.authored_tree
+    count += 1
+
+
+print 'Number of commits in the master: ', count
