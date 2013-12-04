8d279573a76f07c46ea685406eab0814e7766b46
Pradeep Kishore Gowda <pradeep@btbytes.com>
2013-12-03 20:47:41 -0500

    move static files

--- thweb/thweb/views.py
+++ thweb/thweb/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, "home.html", {})
+	return render(request, "home.html", {'ndays': range(1,32), 'nmonths': range(1,13), 'nhours': range(1,25) })
